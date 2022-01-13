72b60d0a13db30cffd3a81399c27a8d02fe7c4c5
Giorgio Elisei <giorgio.elisei@studenti.unicam.it>
2022-01-13 15:10:15 +0100

    first commit

---
+++ project/src/test/java/it/unicam/ids/casotto/CasottoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.ids.casotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
